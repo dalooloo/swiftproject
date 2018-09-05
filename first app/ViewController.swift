547bd4d7f192976937a1271c34677e6bcded323a
Dalex Toussaint <dalextoussaint@dalexs-macbook-pro.local>
2018-09-04 20:08:41 -0400

    as first commit as gitter

--- first app/ViewController.swift
+++ first app/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var buttonCount = 0
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttontap(_ sender: Any) {
+        
+        
+        buttonCount = buttonCount + 1
+        print(buttonCount)
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.yellow
+            myLabel.text = "You have hit it more than 10"
+        }
+        if buttonCount >= 15 {
+            view.backgroundColor = UIColor.green
+            myLabel.text = "You have hit it more than 15"
+        }
+        if buttonCount >= 20 {
+            view.backgroundColor = UIColor.blue
+            myLabel.text = "You have hit it more than 20"
+        }
+        if buttonCount >= 25{
+            view.backgroundColor = UIColor.red
+            myLabel.text = "you have reached the limit"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
